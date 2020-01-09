bb0c83485dc05dade7a0a09c4154b124e35c5f45
antonioaddis <59084137+antonioaddis@users.noreply.github.com>
2020-01-09 15:30:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,5 @@ setup( name='Agilepy',
        install_requires=requires,
        author='Addis Antonio, Baroncelli Leonardo, Parmiggiani Nicolò',
        author_email='antonio.addis@inaf.it leonardo.baroncelli@inaf.it, nicolo.parmiggiani@inaf.it',
-       packages=find_packages("agilepy"),
-       package_dir={ 'agilepy': 'agilepy' }
-    )
+       packages=["agilepy"]
+     )
