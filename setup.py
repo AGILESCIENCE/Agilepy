3611ec55d023b15d021154815198e21a24dce1e7
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2020-03-04 18:54:46 +0100

    Changed version name, from v1.0.0 to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup( name='Agilepy',
-       version='v1.0.0',
+       version='1.0.0',
        author='Baroncelli Leonardo, Addis Antonio, Bulgarelli Andrea, Parmiggiani Nicolò',
        author_email='leonardo.baroncelli@inaf.it, antonio.addis@inaf.it, andrea.bulgarelli@inaf.it, nicolo.parmiggiani@inaf.it',
        packages=find_packages(),
