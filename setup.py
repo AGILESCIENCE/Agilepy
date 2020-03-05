6df50a7f5956c986f387aead2e9c3594826f7ddc
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2020-03-05 10:54:57 +0100

    setup metadata: added ScienceTools version as depdency_link, added license version

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@
 from setuptools import setup, find_packages
 
 
-setup( name='Agilepy',
+setup( name='agilepy',
        version='1.0.0',
        author='Baroncelli Leonardo, Addis Antonio, Bulgarelli Andrea, Parmiggiani Nicolò',
        author_email='leonardo.baroncelli@inaf.it, antonio.addis@inaf.it, andrea.bulgarelli@inaf.it, nicolo.parmiggiani@inaf.it',
        packages=find_packages(),
        package_dir={ 'agilepy': 'agilepy' },
-       include_package_data=True
+       include_package_data=True,
+       license='GPL-3.0',
+       dependency_links=['https://github.com/AGILESCIENCE/AGILE-GRID-ScienceTools-Setup/tarball/master/#egg=BUILDXXX']
      )
