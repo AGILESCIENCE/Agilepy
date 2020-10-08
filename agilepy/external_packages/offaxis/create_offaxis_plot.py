4c8502c9eb8421e9dc935f7c2e8f7cc79cfda080
Antonio Addis <antonio.addis@inaf.it>
2020-10-08 16:19:01 +0200

    first integration

---
+++ agilepy/external_packages/offaxis/create_offaxis_plot.py
@@ -0,0 +1,150 @@
+# Setup-environment:
+# source activate py_mcal_27
+# source /opt/module/heasoft-6.25
+#
+# parameters:
+#
+# 1) time window path, this file must contains rows for different time windows in MJD format
+# 54746.0 54747.0
+# 54915.0 54916.0
+# 2) ra: position of the source
+# 3) dec: position of the source
+# 4) path to Fermi data fits
+# 5) path to AGILE index file (/AGILE_PROC3/DATA_2/INDEX/LOG.log.index)
+# 6) run number
+# 7) zmax: maximum zenith distance of the source to the center of the detector (unit: degrees) (50.0,60.0 ecc)
+# 8) mode: agile || fermi || all
+
+# example
+
+# OUTPUT
+# a list of directory for each time window, format dir_[run]_[tstart]_[tstop]
+# output_[zmax]_[time window path] file for results
+
+
+
+import os,sys
+from fermicheck import *
+from agilecheck import *
+
+import glob
+
+time_window_file = sys.argv[1]
+ra = float(sys.argv[2])
+dec = float(sys.argv[3])
+path_dati_fermi = sys.argv[4]
+path_log_index = sys.argv[5]
+run_number = sys.argv[6]
+zmax = float(sys.argv[7])
+mode = sys.argv[8]
+step = int(sys.argv[9])
+line1 = float(sys.argv[10])
+line2 = float(sys.argv[11])
+
+
+def MET2orbit(tmin, tmax, path_to_LOG, source='MySource', send=True):
+    """
+       This function creates two files:
+        1) MySource_tmin_tmax.txt containing a list of paths to the attitude information for each selected orbit
+        2) merged_list.fits, which is a fits file containing the attitude information, that will be passed to
+           the agilecheck.py script. The merged file will be filtered on-the-fly in order to have always INSTR_STATUS>0.
+    """
+    import os
+    index  = open(path_to_LOG, 'r')
+    filename = str(source)+'_list_of_orbits_'+str(tmin)+'_'+str(tmax)+'.txt'
+    file   = open(filename, 'w')
+    lines  = index.readlines()
+    index_i, index_f = 1e20,1e20
+    for i in range(len(lines)):
+        field = lines[i].split()
+        tstart = field[1]
+        tstop  = field[2]
+        orbit  = field[0]
+        tstart = float(tstart)
+        tstop  = float(tstop)
+        if (tstart <= tmin) & (tstop >= tmin):
+            index_i = i
+        if (tstart <=tmax) & (tstop >= tmax):
+            index_f = i
+        if (i >= index_i) & (i <= index_f):
+            print index_i, index_f, orbit
+            #print >> file, str(orbit)+'[1][MODE > 0 && INSTR_STATUS > 0]' #add row filtering to have good instr_status
+            print >> file, str(orbit)+'[1][LIVETIME > 0 && LOG_STATUS == 0 && MODE == 2 && PHASE .NE. 1 && PHASE .NE. 2]'
+    print "files ", file, " created successfully"
+    file.close()
+    if send == True:
+        os.system("sh "+os.environ['AGILEPIPE']+"/scripts/offaxis/merge_orbit_logs.sh "+str(filename)+" merged_list_"+str(tmin)+"_"+str(tmax)+".fits")
+
+count = -1
+
+f = open("output_"+str(zmax)+"_"+os.path.basename(time_window_file), "a+")
+f.close()
+
+
+
+with open(time_window_file) as fp:
+   for cnt, line in enumerate(fp):
+
+       #count = count + 1
+       #if count != 0:
+        #   continue
+
+
+       tstart = float(line.split()[0])
+       tstop = float(line.split()[1])
+       print("new ----- \n "+str(tstart)+" "+str(tstop))
+
+       f = open("output_"+str(zmax)+"_"+os.path.basename(time_window_file), "a")
+       f.write("\n#####\n")
+       f.write(str(zmax)+" "+str(tstart)+" "+str(tstop)+"\n")
+       f.close()
+
+       #create dir
+       new_dir =  "dir_"+str(run_number)+"_"+str(zmax)+"_"+str(tstart)+"_"+str(tstop)
+       os.mkdir(new_dir)
+       os.chdir(new_dir)
+
+       f = open("output_"+str(zmax)+"_"+os.path.basename(time_window_file), "a")
+       f.write("\n#####\n")
+       f.write(str(zmax)+" "+str(tstart)+" "+str(tstop)+"\n")
+       f.close()
+
+       #### FERMI ####
+
+       if(mode == "fermi" or mode == "all"):
+
+           fermi_met_start = (tstart - 51910.0 ) * 86400.0
+           fermi_met_stop = (tstop - 51910.0 ) * 86400.0
+
+           print("fermi time"+str(fermi_met_start)+" "+str(fermi_met_stop))
+
+           check = fermicheck(path_dati_fermi, ra, dec, tstart, tstop, zmax=zmax, timelimiti=fermi_met_start,step=step, timelimitf=fermi_met_stop,out_name="output_"+str(zmax)+"_"+os.path.basename(time_window_file))
+           check.PlotVisibility()
+           #os.system("cp fermi_visibility*eps "+new_dir)
+
+       #AGILE
+
+       if(mode == "agile" or mode == "all"):
+
+           agile_met_start = (tstart - 53005.0) *  86400.0;
+           agile_met_stop = (tstop - 53005.0) *  86400.0;
+
+           print("agile "+str(agile_met_start)+" "+str(agile_met_stop))
+
+           MET2orbit(agile_met_start, agile_met_stop, path_log_index)
+
+           merged_file = glob.glob('./merged_list_*.fits')[0]
+           check = agilecheck(merged_file, ra, dec, tstart, tstop, zmax=zmax, timelimiti=agile_met_start, timelimitf=agile_met_stop, step=step, out_name="output_"+str(zmax)+"_"+os.path.basename(time_window_file))
+           check.PlotVisibility()
+
+       ### MERGE TWO PLOT ###
+
+       #if(mode == "all"):
+       from merge import *
+       t0 = tstart+((tstop-tstart)/2)
+       t0 = 0
+       check=merge(timelimiti=tstart, timelimitf=tstop, t0=t0,zmax=zmax, lines=[line1, line2])
+       check.Plotmerge(mode=mode)
+       check.histogram_merge(mode=mode)
+
+       os.chdir("..")
---
+++ agilepy/external_packages/offaxis/fermicheck.py
@@ -0,0 +1,363 @@
+import matplotlib.pyplot as plt
+import os
+import pylab
+import numpy as np
+import datetime
+import matplotlib
+from matplotlib import*
+import matplotlib.pyplot as plt
+import astropy
+from astropy.time import Time
+from astropy import coordinates as coord
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+#import aplpy
+import pyfits
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+import bisect
+
+def FermiMET_to_mjd(t_in):
+    """ Returns the variable t_in, expressed Fermi MET seconds, converted to MJD
+        Input: t_in, Fermi MET in seconds
+        Output: t_mjd, Fermi MJD time
+    """
+    tref   = datetime.datetime(2001,1,1,0,0,0)
+    t_temp = tref + datetime.timedelta(seconds=t_in)
+    time2  = Time(t_temp, format='datetime')
+    t_mjd  = time2.mjd
+    return t_mjd
+
+
+
+class fermicheck:
+    """ This class provides a series of calculations and plots to check the off-axis angle of a source wrt Fermi/LAT center FoV.
+
+         Written by: P. Munar-Adrover
+         Version 2.0
+         Last edition: 2016-09-29
+
+        Input parameters:
+        - SC_filename: string, name of the SC fermi file
+        - src_ra: float, right ascension of the source of interest (unit: degrees)
+        - src_dec: float, declination of the source of interest (unit: degrees)
+        - zmax: maximum zenith distance of the source to the center of the detector
+          (unit: degrees)
+        - timelimiti(optional): float, inferior observation time limit to analize.
+          If left blank, timelimiti will be equal to the first "START" time in SC file.
+        - timelimitf (optional): float, inferior observation time limit to analize.
+          If left blank, timelimiti will be equal to the last "STOP" time in SC file.
+        - step (optional): integer, step for selecting the time bins to plot
+
+        Functions within the class:
+        calc_separation
+        calc_mjd
+        calc_mjd_simple
+        WriteTimes
+        PlotVisibility
+    """
+    def __init__(self, SC_filename, src_ra, src_dec, tstart, tstop, zmax=75., timelimiti=-1, timelimitf=-1, step=1,out_name = "output_fermi.txt"):
+        self.SC_filename = SC_filename
+        self.zmax        = zmax
+        self.src_ra      = src_ra
+        self.src_dec     = src_dec
+        self.timelimiti  = timelimiti
+        self.timelimitf  = timelimitf
+        self.step        = step
+        self.out_name = out_name
+        self.tstart = tstart
+        self.tstop = tstop
+
+    def calc_separation(self):
+        """ Function that computes the angular separation between the center of the
+        Fermi/LAT field of view and the coordinates for a given position in the sky,
+        given by src_ra and src_dec.
+            Output: separation (array), time_i (array), time_f (array)
+        """
+        print "Computing angular distance to the center of f.o.v"
+        print"This might take a while..."
+
+        # reading the Attitude Fermi file (the one that finishes with _SC.fits)
+        file     = pyfits.open(self.SC_filename)
+        SC       = file[1].data
+        # if non specified, the initial and final time will be the first and last time in the SC file, respectively
+        if self.timelimiti == -1:
+            self.timelimiti = np.min(SC['START'])
+        if self.timelimitf == -1:
+            self.timelimitf = np.max(SC['STOP'])
+        # selecting the index corresponding to the time prior to the selected timelimiti (in case timelimiti does not
+        # coincide with any given time in the SC file)
+        index_ti = bisect.bisect_left(SC['START'],self.timelimiti)
+        # selecting the index corresponding to the time after to the selected timelimitf (in case timelimitf does not
+        # coincide with any given time in the SC file)
+        index_tf = bisect.bisect_left(SC['STOP'],self.timelimitf)
+
+        # creating arrays filled with zeros
+        src_raz  = np.zeros(len(SC['START'][index_ti:index_tf:int(self.step)]))
+        src_decz = np.zeros(len(SC['START'][index_ti:index_tf:int(self.step)]))
+
+        # filling the just created arrays with our coordinates of interest
+        src_ra   = src_raz + self.src_ra
+        src_dec  = src_decz + self.src_dec
+
+        c1  = SkyCoord(src_ra, src_dec, unit='deg', frame='icrs')
+        c2  = SkyCoord(SC['RA_SCZ'][index_ti:index_tf:int(self.step)], SC['DEC_SCZ'][index_ti:index_tf:int(self.step)], unit='deg', frame='icrs')
+#        print 'c1=', len(c1), 'c2=', len(c2) # to ensure c1 and c2 have the same length
+        sep = c2.separation(c1)
+        return np.asfarray(sep), SC['START'][index_ti:index_tf:int(self.step)], SC['STOP'][index_ti:index_tf:int(self.step)]
+
+    def calc_mjd(self):
+        """
+        Function that transforms a given time in MET (Missin Ellapsed Time) to MJD
+        (Modified Julian Date).
+        Output: t_start (list), t_stop (list)
+        """
+        print "Converting MET to MJD time."
+        print "This might take a while..."
+
+        file     = pyfits.open(self.SC_filename)
+        SC       = file[1].data
+
+        if self.timelimiti == -1:
+            self.timelimiti = np.min(SC['START'])
+        if self.timelimitf == -1:
+            self.timelimitf = np.max(SC['STOP'])
+
+        index_ti = bisect.bisect_left(SC['START'],self.timelimiti)
+        index_tf = bisect.bisect_left(SC['STOP'],self.timelimitf)
+
+        print 'Time_i =', SC['START'][index_ti]
+        print 'Time_f =', SC['STOP'][index_tf]
+
+        tref     = datetime.datetime(2001,1,1,0,0,0)
+        t_start  = []
+        t_stop   = []
+        for i in np.arange(index_ti,index_tf, self.step):
+            time_i       = SC['START'][i]
+            time_f       = SC['STOP'][i]
+            t_start_temp = tref + datetime.timedelta(seconds=time_i)
+            t_stop_temp  = tref + datetime.timedelta(seconds=time_f)
+            tstart       = Time(t_start_temp, format='datetime')
+            tstop        = Time(t_stop_temp, format='datetime')
+            tstart_data  = tstart.mjd
+            tstop_data   = tstop.mjd
+            t_start.append(tstart_data)
+            t_stop.append(tstop_data)
+        return np.asfarray(t_start), np.asfarray(t_stop)
+
+    def calc_mjd_simple(self, t_in):
+        """ Returns the variable t_in, expressed Fermi MET seconds, converted to MJD
+            Input: t_in, Fermi MET in seconds
+            Output: t_mjd, Fermi MJD time
+        """
+        tref   = datetime.datetime(2001,1,1,0,0,0)
+        t_temp = tref + datetime.timedelta(seconds=t_in)
+        time2  = Time(t_temp, format='datetime')
+        t_mjd  = time2.mjd
+        return t_mjd
+
+
+    def WriteTimes(self):
+        """
+        Function that writes the initial and final time into a file, for which
+        the source of interest, at src_ra src_dec coordinates, is at a zenith
+        distance from the center of the camera lower than the zmax value.
+
+        Output: file with columns tiMET, tfMET
+        """
+        ti_mjd, tf_mjd = self.calc_mjd()
+        total_obs = (np.max(tf_mjd)-np.min(ti_mjd))//1
+        meantime = (np.asfarray(ti_mjd)+np.asfarray(tf_mjd))/2.
+        separation, tiMET, tfMET = self.calc_separation()
+        ti, tf = np.min(tiMET), np.max(tfMET)
+        gti_file = open("gtis_file_ra"+str(self.src_ra)+"_dec"+str(self.src_dec)+"_"+str(ti)+"_"+str(tf)+".txt","w")
+        lenght   = len(separation[separation < self.zmax])
+
+        for i in np.arange(0,lenght):
+            print >> gti_file, tiMET[i], tfMET[i]
+
+
+    def PlotVisibility(self, twocolumn=False, show=False, im_fmt='eps', histogram=True, plot=True):
+        """PlotVisibility makes a plot of the zenith distance of a given source
+           (src_ra and src_dec in the fermivisibility parameters). For a given
+           zmax angle it plots in red the time when the source zenith distance
+           is below zmax value, and in blue the time when the source is at zenith
+           distance larger than zmax.
+
+           Input (optional) parameters:
+           - twocolumn: boolean, True or False (default). If two column=True, the
+             plot is adjusted to the size of a two column journal publication.
+           - show: boolean, True or False (default). If set to True the plot is
+             displayed in the interactive ipython plotting window.
+           - im_fmt: string, desired format for the image to be stored (if show=False
+             is selected). Current supported formats are: 'eps', 'jpg', 'png', 'ps', 'pdf'.
+           - histogram: boolean, True or False (default). If set to True a histogram of
+             the percentage of time spent at diferent zenith distance bins (10 degrees bins)
+             is saved.
+        """
+#=================================================================
+#====================== plotting settings ========================
+#=================================================================
+
+        if twocolumn == True:
+            fig_width_pt = 255.76535
+            fontsize     = 12
+        else:
+            fig_width_pt = 426.79134
+            fontsize     = 10
+
+
+        inches_per_pt = 1.0/72.27               # Convert pt to inch
+        golden_mean = (np.sqrt(5)+1.0)/2.0         # Aesthetic ratio
+        fig_width = fig_width_pt*inches_per_pt  # width in inches
+        fig_height = fig_width/golden_mean      # height in inches
+        fig_size =  [fig_width,fig_height]
+
+        params = {'backend': 'pdf',
+            'axes.labelsize': fontsize,
+            'font.size': fontsize,
+            'legend.fontsize': fontsize,
+            'xtick.labelsize': fontsize,
+            'ytick.labelsize': fontsize,
+            'axes.linewidth': 0.5,
+            'lines.linewidth': 0.5,
+            #'text.usetex': True,
+            'ps.usedistiller': False,
+            'figure.figsize': fig_size,
+            'font.family': 'Times New Roman',
+            'font.serif': ['Bitstream Vera Serif']}
+        pylab.rcParams.update(params)
+#================== end of plotting settings =====================
+
+        separation, tiMET, tfMET = self.calc_separation()
+        ti_mjd = []
+        tf_mjd = []
+
+        for i in np.arange(len(tiMET)):
+            timjd = self.calc_mjd_simple(tiMET[i])
+            tfmjd = self.calc_mjd_simple(tfMET[i])
+            ti_mjd.append(timjd)
+            tf_mjd.append(tfmjd)
+
+        ti_mjd = np.array(ti_mjd)
+        tf_mjd = np.array(tf_mjd)
+
+        total_obs = (np.max(tf_mjd)-np.min(ti_mjd))//1
+        meantime = (ti_mjd+tf_mjd)/2.
+
+        deltat = tfMET - tiMET
+        daltat1 = deltat[1]
+
+        ttotal_obs = np.sum(deltat)
+        ttotal_under_zmax = np.sum(tfMET[separation<self.zmax]-tiMET[separation<self.zmax])
+        ttotal_above_zmax = np.sum(tfMET[separation>self.zmax]-tiMET[separation>self.zmax])
+        print "Total integration time=", "{0:.2f}".format((ttotal_obs*self.step)), " s", 'Total_bins:', total_obs, len(tiMET), 'Mean sep. < ', self.zmax, ':', str(round(np.mean(separation[separation<self.zmax]),4))
+        print "Total absolute time=","{0:.2f}".format((self.tstop-self.tstart )*86400) #NEW
+        print "Total time spent at separation < ", self.zmax, " deg:", "{0:.2f}".format(ttotal_under_zmax*self.step), "s"
+        print "Relative time spent at separation <", self.zmax, " deg:", "{0:.2f}".format(ttotal_under_zmax*100./ttotal_obs), "%"
+        print "Relative time spent at separation >", self.zmax, " deg:", "{0:.2f}".format(ttotal_above_zmax*100./ttotal_obs), "%"
+        print "Absolute time spent at separation <", self.zmax, " deg:", "{0:.2f}".format((ttotal_under_zmax*self.step)/((self.tstop-self.tstart )*86400)*100), " %" #NEW
+        print "Absolute time spent at separation >", self.zmax, " deg:", "{0:.2f}".format(((ttotal_obs*self.step)-(ttotal_under_zmax*self.step)) / ((self.tstop-self.tstart )*86400)*100), " %" #NEW
+        print "Duty Cycle: ", "{0:.2f}".format((((ttotal_obs*self.step) / ((self.tstop-self.tstart )*86400)))*100), "%" #NEW
+
+        f = open(self.out_name, "a")
+        print >> f, "FERMI"
+        print >> f,"Total integration time=", "{0:.2f}".format((ttotal_obs*self.step)), " s", 'Total_bins:', total_obs, len(tiMET), 'Mean sep. < ', self.zmax, ':', str(round(np.mean(separation[separation<self.zmax]),4))
+        print >> f,"Total absolute time=","{0:.2f}".format((self.tstop-self.tstart )*86400) #NEW
+        print >> f,"Total time spent at separation < ", self.zmax, " deg:", "{0:.2f}".format(ttotal_under_zmax*self.step), "s"
+        print >> f,"Relative time spent at separation <", self.zmax, " deg:", "{0:.2f}".format(ttotal_under_zmax*100./ttotal_obs), "%"
+        print >> f,"Relative time spent at separation >", self.zmax, " deg:", "{0:.2f}".format(ttotal_above_zmax*100./ttotal_obs), "%"
+        print >> f,"Absolute time spent at separation <", self.zmax, " deg:", "{0:.2f}".format((ttotal_under_zmax*self.step)/((self.tstop-self.tstart )*86400)*100), " %" #NEW
+        print >> f,"Absolute time spent at separation >", self.zmax, " deg:", "{0:.2f}".format(((ttotal_obs*self.step)-(ttotal_under_zmax*self.step)) / ((self.tstop-self.tstart )*86400)*100), " %" #NEW
+        print >> f,"Duty Cycle: ", "{0:.2f}".format((((ttotal_obs*self.step) / ((self.tstop-self.tstart )*86400)))*100), "%" #NEW
+        f.close()
+
+        filesep = open('time_vs_separation_fermi.txt', 'w')
+        l = len(separation)
+        for i in np.arange(l):
+            print >> filesep, meantime[i], separation[i]
+        filesep.close()
+
+# This commented lines where to plot grey shade in the corresponding detected flares by AGILE for MWC 656
+#        agile_i = np.array([217468930.0, 236347395.0, 252763332.0, 298123266.0, 299548866.0, 301708933.0, 324000132.0, 339724932.0, 384307200.0, 395107200.0])
+#        agile_f = np.array([217598530.0, 236498595.0, 252979332.0, 298231266.0, 299721666.0, 301881732.0, 324172932.0, 339897732.0, 384393600.0, 395280000.0])
+
+#        tref     = datetime.datetime(2004,1,1,0,0,0)
+#        t_agilestart  = []
+#        t_agilestop   = []
+#        for i in np.arange(len(agile_i)):
+#            time_i       = agile_i[i]
+#            time_f       = agile_f[i]
+#            t_start_temp = tref + datetime.timedelta(seconds=time_i)
+#            t_stop_temp  = tref + datetime.timedelta(seconds=time_f)
+#            tstart       = Time(t_start_temp, format='datetime')
+#            tstop        = Time(t_stop_temp, format='datetime')
+#            tstart_data  = tstart.mjd
+#            tstop_data   = tstop.mjd
+#            t_agilestart.append(tstart_data)
+#            t_agilestop.append(tstop_data)
+
+#        t_agilei_mjd = np.asfarray(t_agilestart)
+#        t_agilef_mjd = np.asfarray(t_agilestop)
+
+#        for i in np.arange(len(t_agilei_mjd)):
+#            ax.fill_between([t_agilei_mjd[i], t_agilef_mjd[i]], 0, 200, color='grey', alpha=0.5)
+
+        if plot == True:
+            print 'Plotting figure...'
+            f  = plt.figure()
+            ax = f.add_subplot(111)
+            ax.plot(meantime, separation, '-b')
+#            ax.plot(meantime[separation < self.zmax], separation[separation < self.zmax], '-r')
+
+            ax.set_ylim(0., self.zmax+5.0)
+            ax.set_xlabel('MJD')
+            ax.set_ylabel('off-axis angle [$^{\\circ}$]')
+
+            print 'Saving figure...'
+            ax.set_xlim(np.min(meantime), np.max(meantime))
+            if show==True:
+                f.show()
+            else:
+                f.savefig('fermi_visibility_ra'+str(self.src_ra)+'_dec'+str(self.src_dec)+'_tstart'+str(np.min(tiMET))+'_tstop'+str(np.max(tfMET))+'.'+str(im_fmt))
+
+        if histogram == True:
+            print "Plotting histogram..."
+            bins  = [0, 10, 20, 30, 40, 50, 60, 70]
+            bins2 = [70, 180]
+            hist, bins = np.histogram(separation, bins=bins, density=False)
+            hist2, bins2 = np.histogram(separation, bins=bins2, density=False)
+            width = 1. * (bins[1] - bins[0])
+            center = (bins[:-1] + bins[1:]) / 2
+            width2 = 1. * (bins2[1] - bins2[0])
+            center2 = (bins2[:-1] + bins2[1:]) / 2
+#            print hist, bins, center, width
+#            print hist2, bins2, center2, width2
+#            print len(separation)
+
+            f2  = plt.figure()
+            ax2 = f2.add_subplot(111)
+            ax2.bar(center, hist*29.1058/ttotal_obs*100., align='center', color='w', edgecolor='b', width=width)
+            ax2.bar(center2, hist2*29.1058/ttotal_obs*100., align='center', color='w', edgecolor='b', width=width2)
+
+            ax2.set_xlim(0., 100.)
+            ax2.set_ylim(0., 100.)
+            ax2.set_ylabel('\\% of time spent')
+            ax2.set_xlabel('off-axis angle $[^\\circ]$')
+            labels  = [0, 10, 20, 30, 40, 50, 60, 180]
+            xlabels = [0, 10, 20, 30, 40, 50, 60, 100]
+            plt.xticks(xlabels, labels)
+
+            #fil = open('fermi_histogram_visibility'+str(self.src_ra)+'_dec'+str(self.src_dec)+'_tstart'+str(np.min(tiMET))+'_tstop'+str(np.max(tfMET))+'.txt', 'w')
+            fil = open('fermi_histogram_visibility.txt', 'w')
+            for i in np.arange(len(center)):
+                print >> fil, center[i], hist[i]*29.1058/ttotal_obs*100., width
+            for i in np.arange(len(center2)):
+                print >> fil, center2[i], hist2[i]*29.1058/ttotal_obs*100., width2
+            fil.close()
+
+            print 'Saving figure...'
+            if show==True:
+                f2.show()
+            else:
+                f2.savefig('fermi_histogram_ra'+str(self.src_ra)+'_dec'+str(self.src_dec)+'_tstart'+str(np.min(tiMET))+'_tstop'+str(np.max(tfMET))+'.'+str(im_fmt))
+            return ttotal_obs, np.concatenate((hist,hist2))
