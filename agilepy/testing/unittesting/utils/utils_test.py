75d2ab54c3c94984196fbfd2885c7e594b15e3f6
Antonio Addis <antonio.addis@inaf.it>
2020-11-12 14:08:59 +0100

    CI Bugfix

--- agilepy/testing/unittesting/utils/utils_test.py
+++ agilepy/testing/unittesting/utils/utils_test.py
@@ -135,19 +135,6 @@ class AgilepyUtilsUT(unittest.TestCase):
 
         self.assertEqual(True, os.path.isfile(file))
 
-    def test_display_light_curve(self):
-
-        pu = PlottingUtils(self.config, self.agilepyLogger)
-
-        file_lc = self.datadir+"/lc-4.txt"
-
-        self.assertRaises(ValueError, pu.plotLc, file_lc, 1500, 1000, True)
-
-        #with self.assertRaises(ValueError, pu.plotLc, file_lc, 1500, 1000, True) as cm:
-        #        input("..")
-        #            print(cm)
-
-        # self.assertEqual(True, os.path.isfile(file))
 
     def test_initialize_logger_verboselvl_2(self):
         sleep(1.0)
