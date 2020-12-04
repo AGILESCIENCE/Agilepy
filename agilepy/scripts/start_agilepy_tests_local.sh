36e3f21f75dd5e54db9a8a28a4a5c225be8163df
Antonio Addis <antonio.addis@inaf.it>
2020-12-04 18:38:54 +0100

    bugfix tests scripts

--- agilepy/scripts/start_agilepy_tests_local.sh
+++ agilepy/scripts/start_agilepy_tests_local.sh
@@ -14,5 +14,5 @@ python "$script_dir/../testing/unittesting/api/ag_analysis_unit_test.py" -v
 python "$script_dir/../testing/unittesting/api/sources_library_unit_test.py" -v
 python "$script_dir/../testing/unittesting/config/agilepy_ag_analysis_config_test.py" -v
 python "$script_dir/../testing/unittesting/utils/utils_test.py" -v
-python "$agilepy_path/testing/unittesting/api/ag_analysis_wavelet_unit_test.py" -v
-python "$agilepy_path/testing/unittesting/api/ag_eng_agile_offaxis_visibility_unit_test.py" -v
+python "$script_dir/../testing/unittesting/api/ag_analysis_wavelet_unit_test.py" -v
+python "$script_dir/../testing/unittesting/api/ag_eng_agile_offaxis_visibility_unit_test.py" -v
