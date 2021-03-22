3ecea6d66045733ef8adbbc99ea161612c3fbe86
Antonio Addis <antonio.addis@inaf.it>
2021-03-22 12:20:47 +0100

    bugfix #262 done

--- agilepy/utils/PlottingUtils.py
+++ agilepy/utils/PlottingUtils.py
@@ -399,25 +399,28 @@ class PlottingUtils(metaclass=Singleton):
         data["x_minus"] = data["tm"] - data["time_start_mjd"]
         sel1 = data.loc[data["sqrt(ts)"] >= 3]
         sel2 = data.loc[data["sqrt(ts)"] < 3]
-
         #Plotting
         fig = go.Figure()
         fig.add_traces(go.Scatter(x=sel1["tm"], y=sel1["flux"],
                                   error_x=dict(type="data", symmetric=False, array=sel1["x_plus"],
                                                arrayminus=sel1["x_minus"]),
                                   error_y=dict(type="data", symmetric=True, array=sel1["flux_err"]), mode="markers",
-                                  hovertemplate="Time: %{x:.1f}, flux: %{y:.2f} +/- %{error_y.array:.2f}", name="sqrts >=3"))
+                                  customdata = np.stack((sel1["time_start_mjd"],
+                                             sel1["time_end_mjd"]), axis=-1),
+                                  hovertemplate="tstart: %{customdata[0]:.4f} - tend:%{customdata[1]:.4f}, flux: %{y:.2f} +/- %{error_y.array:.2f}", name="sqrts >=3"))
         fig.add_traces(go.Scatter(x=sel2["tm"], y=sel2["flux_ul"],
                                   error_x=dict(type="data", symmetric=False, array=sel2["x_plus"],
                                                arrayminus=sel2["x_minus"]), mode='markers',
-                                  hovertemplate="Time: %{x:.1f}, flux_ul: %{y:.2f}",
+                                  hovertemplate="tstart: %{customdata[0]:.4f}, tend:%{customdata[1]:.4f}, flux_ul: %{y:.2f}",
+                                  customdata=np.stack((sel2["time_start_mjd"],
+                                                      sel2["time_end_mjd"]), axis=-1),
                                   marker_symbol="triangle-down", marker_size=10, name="sqrts < 3"))
 
         if lineValue is not None and lineError is not None:
             fig.add_traces(go.Scatter(x=data["tm"], y=[lineValue] * len(data["tm"]), error_y= dict(type="constant", value=lineError), line=dict(dash="dash"), name="line1"))
 
 
-        fig.update_xaxes(showline=True, linecolor="black", title="Time(mjd)")
+        fig.update_xaxes(showline=True, linecolor="black", title="Time(MJD)")
         fig.update_yaxes(showline=True, linecolor="black", title=r"$10^{-8} ph cm^{-2} s^{-1}$")
         fig.update_layout(legend=dict(font=dict(size=20)), xaxis=dict(tickformat="g"))
         
