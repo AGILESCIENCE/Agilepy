eaaf0742f096a8525c7077da2deecd9ae69b8cd6
Leonardo Baroncelli <51923487+LeoBaro@users.noreply.github.com>
2022-02-25 16:49:25 +0100

    Install sh scripts and data files

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# -*- coding: latin-1 -*-
 
 from setuptools import setup, find_packages
 
@@ -9,6 +8,22 @@ setup( name='agilepy',
        author_email='leonardo.baroncelli@inaf.it, antonio.addis@inaf.it, andrea.bulgarelli@inaf.it, nicolo.parmiggiani@inaf.it',
        packages=find_packages(),
        package_dir={ 'agilepy': 'agilepy' },
+       scripts=[
+          'agilepy/scripts/start_agilepy_notebooks.sh',
+          'agilepy/scripts/start_container.sh',
+          'agilepy/scripts/start_coverage_local.sh',
+          'agilepy/scripts/start_coverage.sh',
+          'agilepy/scripts/start_notebooks_docker.sh'
+       ],
        include_package_data=True,
+       package_data={'mypkg': [
+                         'testing/unittesting/api/conf/*',
+                         'testing/unittesting/api/data/*',
+                         'testing/unittesting/config/conf/*',
+                         'testing/unittesting/core/conf/*',
+                         'testing/unittesting/core/test_data/*',
+                         'testing/unittesting/utils/conf/*',
+                         'testing/unittesting/utils/test_data/*'                         
+                    ]},
        license='GPL-3.0'
      )
