346c846dc43e3a4db383384ee3ba77dad8b7ff0b
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2023-04-29 00:25:16 +0200

    Fix some medium and critical code quality issues

--- agilepy/config/ValidationStrategies.py
+++ agilepy/config/ValidationStrategies.py
@@ -25,7 +25,6 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from os import stat, listdir
 from pathlib import Path
 from typing import List
 import numpy as np
--- agilepy/testing/unittesting/utils/test_utils_dates.py
+++ agilepy/testing/unittesting/utils/test_utils_dates.py
@@ -30,7 +30,7 @@ def test_mjd_conversion(input_date, expected):
     (1643379772, {"agile_seconds":570464572,"jd":2459608.09921296,"mjd":59607.59921296,"fits":"2022-01-28T14:22:52.000","iso":"2022-01-28 14:22:52.000"}),
     (1522497532, {"agile_seconds":449582332,"jd":2458208.99921296,"mjd":58208.49921296,"fits":"2018-03-31T11:58:52.000","iso":"2018-03-31 11:58:52.000"})])
 
-def test_unix_conversion(input_date, expected):
+def test_unix_conversion_1(input_date, expected):
 
     assert AstroUtils.time_unix_to_agile_seconds(input_date) == expected["agile_seconds"]
     assert AstroUtils.time_unix_to_jd(input_date) == pytest.approx(expected["jd"], 0.00001)
@@ -42,7 +42,7 @@ def test_unix_conversion(input_date, expected):
     ("2022-01-28T14:22:52.000", {"agile_seconds":570464572,"jd":2459608.09921296,"mjd":59607.59921296,"unix":1643379772,"iso":"2022-01-28 14:22:52.000"}),
     ("2018-03-31T11:58:52.000", {"agile_seconds":449582332,"jd":2458208.99921296,"mjd":58208.49921296,"unix":1522497532,"iso":"2018-03-31 11:58:52.000"})])
 
-def test_unix_conversion(input_date, expected):
+def test_unix_conversion_2(input_date, expected):
 
     assert AstroUtils.time_fits_to_agile_seconds(input_date) == expected["agile_seconds"]
     assert AstroUtils.time_fits_to_jd(input_date) == pytest.approx(expected["jd"], 0.00001)
@@ -54,7 +54,7 @@ def test_unix_conversion(input_date, expected):
     ("2022-01-28 14:22:52.000", {"agile_seconds":570464572,"jd":2459608.09921296,"mjd":59607.59921296,"unix":1643379772,"fits":"2022-01-28T14:22:52.000"}),
     ("2018-03-31 11:58:52.000", {"agile_seconds":449582332,"jd":2458208.99921296,"mjd":58208.49921296,"unix":1522497532,"fits":"2018-03-31T11:58:52.000"})])
 
-def test_unix_conversion(input_date, expected):
+def test_unix_conversion_3(input_date, expected):
 
     assert AstroUtils.time_iso_to_agile_seconds(input_date) == expected["agile_seconds"]
     assert AstroUtils.time_iso_to_jd(input_date) == pytest.approx(expected["jd"], 0.00001)
@@ -66,7 +66,7 @@ def test_unix_conversion(input_date, expected):
     (570464572, {"iso":"2022-01-28 14:22:52.000","jd":2459608.09921296,"mjd":59607.59921296,"unix":1643379772,"fits":"2022-01-28T14:22:52.000"}),
     (449582332, {"iso":"2018-03-31 11:58:52.000","jd":2458208.99921296,"mjd":58208.49921296,"unix":1522497532,"fits":"2018-03-31T11:58:52.000"})])
 
-def test_unix_conversion(input_date, expected):
+def test_unix_conversion_4(input_date, expected):
 
     assert AstroUtils.time_agile_seconds_to_iso(input_date) == expected["iso"]
     assert AstroUtils.time_agile_seconds_to_jd(input_date) == pytest.approx(expected["jd"], 0.00001)
