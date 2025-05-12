7a7cdaf7017a53705cb16c9d0bcb6ef7f25596d8
Gabriele.Panebianco <gabriele.panebianco@cta-consortium.org>
2025-05-12 14:32:41 +0200

    test commit with name bugfix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ entry_points = {
 
 
 setup( name='agilepy',
-       version='1.6.3',
+       version='1.6.4',
        author='Baroncelli Leonardo, Addis Antonio, Bulgarelli Andrea, Parmiggiani Nicolò, Ambra Di Piano, Gabriele Panebianco',
        author_email='leonardo.baroncelli@inaf.it, antonio.addis@inaf.it, andrea.bulgarelli@inaf.it, nicolo.parmiggiani@inaf.it, ambra.dipiano@inaf.it, gabriele.panebianco@inaf.it',
        packages=find_packages(),
